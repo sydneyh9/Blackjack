47e9a409ca80f210aa21fe2fb95afc9d77c0346f
Sydney Holland <sydneymh@udel.edu>
2025-08-10 09:54:53 -0400

    Working on win or lose structure

--- play.js
+++ play.js
@@ -13,12 +13,14 @@ let dealer_score = 0;
 let dealer_choice = 0;
 let final_hand = "";
 let computer_final_hand = "";
+let win_or_lose = "";
 
 //establish button for dealing the cards
 const button = document.getElementById('deal');
 const yourcards = document.getElementById('your_cards');
 const currentscore = document.getElementById('current_score');
 const dealerfirstcard = document.getElementById('dealer_first_card');
+const winorlose = document.getElementById('win_or_lose');
 
 function onButtonClick() {
     //the dealer gets their cards
@@ -41,12 +43,22 @@ your_cards.push(first_card, second_card);
 
 //update scores
 current_score = first_card + second_card;
+
 dealer_score = dealer_first_card + dealer_second_card;
+
+//win or lose skeleton
+if (current_score > 21) {
+    win_or_lose = "You went over. You lose!";
+    console.log("Your Final Hand: ", your_cards);
+    console.log("Your Final Score: ", current_score);
+    console.log("You went over. You lose!");
+}
 
 //update our display
 currentscore.textContent = `Current Score: ${current_score}`;
 yourcards.textContent = `Your Cards: ${your_cards.join(',')}`;
 dealerfirstcard.textContent = `Dealer's First Card: ${dealer_first_card}`;
+winorlose.textContent = `Result: ${win_or_lose}`;
 console.log("Your cards:", your_cards);
 console.log("Dealer's first card:",dealer_first_card);
 console.log("Current Score:", current_score);
